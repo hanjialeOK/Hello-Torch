8c56308bbb3e306736a92916661f94d868a4dfbc
hanjiale <hanjiale@mail.ustc.edu.cn>
2021-05-07 10:36:55 +0800

    PoolNet is available now!

--- hellotorch.cpp
+++ hellotorch.cpp
@@ -2,51 +2,12 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
+#include <chrono>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
 
-#include "networks/deeplab_resnet.h"
-
-struct NetImpl : torch::nn::Module {
-    NetImpl(int k) : 
-        conv1(torch::nn::Conv2dOptions(k, 256, 4)
-                .bias(false)),
-        batch_norm1(256),
-        conv2(torch::nn::Conv2dOptions(256, 128, 3)
-                .stride(2)
-                .padding(1)
-                .bias(false)),
-        batch_norm2(128),
-        conv3(torch::nn::Conv2dOptions(128, 64, 4)
-                .stride(2)
-                .padding(1)
-                .bias(false)),
-        batch_norm3(64),
-        conv4(torch::nn::Conv2dOptions(64, 1, 4)
-                .stride(2)
-                .padding(1)
-                .bias(false))
-    {
-        // register_module() is needed if we want to use the parameters() method later on
-        register_module("conv1", conv1);
-        register_module("conv2", conv2);
-        register_module("conv3", conv3);
-        register_module("conv4", conv4);
-        register_module("batch_norm1", batch_norm1);
-        register_module("batch_norm2", batch_norm2);
-        register_module("batch_norm3", batch_norm3);
-    }
-
-    torch::Tensor forward(torch::Tensor x) {
-        x = torch::relu(batch_norm1(conv1(x)));
-        x = torch::relu(batch_norm2(conv2(x)));
-        x = torch::relu(batch_norm3(conv3(x)));
-        x = torch::tanh(conv4(x));
-        return x;
-    }
-
-    torch::nn::Conv2d conv1, conv2, conv3, conv4;
-    torch::nn::BatchNorm2d batch_norm1, batch_norm2, batch_norm3;
-};
-TORCH_MODULE(Net);
+#include "networks/poolnet.h"
 
 int main() {
     torch::Device device = torch::kCPU;
@@ -54,40 +15,100 @@ int main() {
         std::cout << "CUDA is available! Training on GPU." << std::endl;
         device = torch::kCUDA;
     }
-    // Net net(100);
-    ResNet net = resnet50();
+    PoolNet net = PoolNet();
     net->to(device);
     std::cout << "loading weights..." << std::endl;
-    torch::load(net, "../models/resnet50_caffe.pt");
+    torch::load(net, "../models/poolnet.pt");
     std::cout << "weights have been loaded!" << std::endl;
     net->eval();
 
-    for (const auto& p : net->parameters()) {
-        // p.requires_grad_(false);
-    }
+    // for (const auto& p : net->parameters()) {
+    //     p.requires_grad_(false);
+    // }
     for (const auto& pair : net->named_parameters()) {
         // std::cout << pair.key() << ": " << pair.value() << std::endl;
-        std::cout << pair.key() << " " << pair.value().requires_grad() << std::endl;
+        // std::cout << pair.key() << " " << pair.value().requires_grad() << std::endl;
     }
 
     int input_size = 224;
     cv::Mat img;
 
-    img = cv::imread("../images/1.jpg");
+	DIR *dir = NULL;
+	struct dirent *file;
+	if((dir = opendir("../images/")) == NULL) {  
+		printf("opendir failed!");
+		return -1;
+	}
 
-    cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
-    cv::resize(img, img, cv::Size(input_size, input_size));
+	int cnt = 0;
+    while(file = readdir(dir)) {
+		// 判断是否为文件
+		if (file->d_type != 8) continue;
 
-    // auto img_tensor = torch::from_blob(img.data, {1, img.cols, img.rows, 3}, torch::kByte).to(device);
-    // auto img_tensor = torch::from_blob(img.data, {1, input_size, input_size, 3}, torch::kByte).to(device);
-    // img_tensor = img_tensor.permute({0,3,1,2});
-    // img_tensor = img_tensor.toType(torch::kFloat);
-    // img_tensor = img_tensor.div(255.0);
-    auto img_tensor = torch::ones({1, 3, 224, 224}, torch::kFloat).to(device);
+        char name[20] = "\0", type[5] = "\0";
+        strncpy(name, file->d_name, 11*sizeof(char));
+        name[11] = 0;
+        strncpy(type, file->d_name + 12, 3*sizeof(char));
+        type[3] = 0;
 
-    auto out = net->forward(img_tensor);
+        if (strcmp(type, "jpg") != 0) continue;
 
-    std::cout << out[0][2000] << std::endl;
+		std::cout << file->d_name << std::endl;
+		// 为文件加上相对路径
+		char fileName[30] = "../images/";
+		strcat(fileName, file->d_name);
 
+        img = cv::imread(fileName);
+
+        cv::cvtColor(img, img, cv::COLOR_BGR2RGB);
+        // cv::resize(img, img, cv::Size(input_size, input_size));
+
+        auto img_tensor = torch::from_blob(img.data, {1, img.rows, img.cols, 3}, torch::kByte).to(device);
+        img_tensor = img_tensor.permute({0,3,1,2});
+        img_tensor = img_tensor.toType(torch::kFloat);
+        // img_tensor = img_tensor.div(255.0);
+        // img_tensor = img_tensor.detach();
+
+        auto start = std::chrono::high_resolution_clock::now();
+
+        torch::NoGradGuard no_grad;
+        auto out = net->forward(img_tensor);
+        std::cout << out.type() << " " << out.sizes() << " " << out.requires_grad() << std::endl;
+        // out = out.detach();
+
+        // out.squeeze_(0);
+        out.squeeze_();
+        out.sigmoid_();
+        out.mul_(255.0);
+        out = out.toType(torch::kByte);
+        // out = out.permute({1, 2, 0});
+        // out = out.expand({out.size(0), out.size(1), 3});
+        // std::cout << "is_contiguous(out): " << out.is_contiguous() << std::endl;
+        // out = out.contiguous();
+        // out = out.detach();
+
+        auto start1 = std::chrono::high_resolution_clock::now();
+        out = out.to(torch::kCPU);
+        std::cout << out.type() << " " << out.sizes() << " " << out.requires_grad() << std::endl;
+        auto end1 = std::chrono::high_resolution_clock::now();
+        auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>(end1 - start1);
+        // It should be known that it takes longer time at first time
+        std::cout << "GPU to CPU " << duration1.count() << " ms" << std::endl;
+
+        cv::Mat pred;
+        pred.create(cv::Size(img.cols, img.rows), CV_8UC1);
+        memcpy(pred.data, out.data_ptr(), out.numel() * sizeof(torch::kByte));
+
+        auto end = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        // It should be known that it takes longer time at first time
+        std::cout << "inference taken : " << duration.count() << " ms" << std::endl;
+
+        char saveName[30] = "../images/";
+		strcat(saveName, name);
+        strcat(saveName, "_cpp.png");
+        cv::imwrite(saveName, pred);
+	}
+    closedir(dir);
     return 0;
 }
